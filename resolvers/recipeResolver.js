0c3da4f44c0ed99079e7770f19f64293b0a74636
Tatsapat Saerejittima <sabaiprimo@gmail.com>
2021-04-18 23:41:40 +0300

    Add filter to recipe resolver

--- resolvers/recipeResolver.js
+++ resolvers/recipeResolver.js
@@ -2,8 +2,31 @@ import Recipe from '../models/recipe.js';
 
 export default {
   Query: {
-    recipes: () => {
-      return Recipe.find();
+    // recipes: () => {
+    //   return Recipe.find();
+    // },
+    recipes: async (parent, args, context, info) => {
+      const { filter } = args;
+      const shouldApplyFilters = filter !== null;
+
+      if (!shouldApplyFilters) {
+        return await Recipe.find();
+      }
+
+      // const shouldApplyTag = filter.tags !== null;
+      const shouldApplyName = filter.title !== null;
+
+      // if (shouldApplyTag) {
+      //   return await Recipe.find({tags: { "$in" : filter.tags]} })
+      // }
+
+      if (shouldApplyName) {
+        const name = filter.title;
+        const regex = new RegExp(name, 'i'); // i for case insensitive
+        return await Recipe.find({ title: { $regex: regex } });
+      }
+
+      return await Recipe.find();
     },
   },
 };
